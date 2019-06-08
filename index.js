4f434788100e326193dc642c526b7e59a8079723
Joshua Choi <jsc.r839@gmail.com>
2019-06-08 20:07:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,17 @@ function destructivelyPrependKitten(name){
   kittens.unshift(name);
   return kittens;
 }
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name){
+  
+}
